2824d9308734ac2375334c2e7d4d736f728de404
Nikita Tumanov <nikita.tumanov@henkel.com>
2020-04-21 13:37:53 +0500

    initial commit

--- checklist/filters.py
+++ checklist/filters.py
@@ -5,7 +5,7 @@ from .models import Questionnaire
 
 
 class QuestionnaireFilter(django_filters.FilterSet):
-    date = django_filters.DateFromToRangeFilter(field_name='date', lookup_expr='iexact', widget=RangeWidget(attrs={'placeholder': 'YYYY-MM-DD','class':'form-control'}))
+    date = django_filters.DateFromToRangeFilter(field_name='date', lookup_expr='iexact', widget=RangeWidget(attrs={'placeholder': 'DD.MM.YYYY','class':'form-control'}))
 
     class Meta:
         model = Questionnaire
